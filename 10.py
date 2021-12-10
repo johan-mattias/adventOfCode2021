a286166d9101687172a0e7f76fd35e53650d9f97
johan-mattias <johan.snider@gmail.com>
2021-12-10 16:41:59 +0100

    it's crarzy how easy stuff like this is when you have already worked on similiar problems

--- 10.py
+++ 10.py
@@ -1,14 +1,21 @@
 from sys import stdin
 
 score= 0
-
-valid = []
-
-def comp(char):
+auto_score=[]
+def switch(char):
+    if char == '(':
+        return ')'
+    elif char == '[':
+        return ']'
+    elif char == '{':
+        return '}'
+    elif char == '<':
+        return '>'
 
 for line in stdin:
     flag = True
     parse=[]
+    auto_temp = 0
 
     for char in line.rstrip():
         if (char == '(' or char == '[' or
@@ -22,6 +29,7 @@ for line in stdin:
             else:
                 print('expected', '(', 'found', pop)
                 score+=3
+                flag = False
                 break
         if char == ']':
             pop = parse.pop()
@@ -30,6 +38,7 @@ for line in stdin:
             else:
                 print('expected', '[', 'found', pop)
                 score+=57
+                flag = False
                 break
         if char == '}':
             pop = parse.pop()
@@ -38,6 +47,7 @@ for line in stdin:
             else:
                 print('expected', '{', 'found', pop)
                 score+=1197
+                flag = False
                 break
         if char == '>':
             pop = parse.pop()
@@ -46,5 +56,22 @@ for line in stdin:
             else:
                 print('expected', '<', 'found', pop)
                 score+=25137
+                flag = False
                 break
-print(score)
+    if(flag):
+        print(parse)
+        print([switch(char) for char in parse[::-1]])
+        for char in parse[::-1]:
+            char = switch(char)
+            auto_temp *= 5
+            if char == ')':
+                auto_temp+= 1
+            elif char == ']':
+                auto_temp+=2
+            elif char == '}':
+                auto_temp +=3
+            elif char == '>':
+                auto_temp+= 4
+        auto_score.append(auto_temp)
+
+print(sorted(auto_score)[len(auto_score)//2])
