ae3c9d24c765784327559a1a5b769f759ed4be9b
Johan Mattias Snider <johan.snider@gmail.com>
2021-12-11 11:25:33 +0100

    oh feeling better aboutt his, it was really nice to put the adjacent points in a list and then just for loop around them

--- 11.py
+++ 11.py
@@ -12,63 +12,46 @@ data.append(padding)
 pprint(data)
 
 flashes = 0
+flashed =[]
 
-def spiral(y, x):
-    if data[y+1][x+1] != -1:
-        data[y+1][x+1]+=1
-        if(data[y+1][x+1] >= 10):
-            data[y+1][x+1] = 0
-            spiral(y+1,x+1)
-    if data[y+1][x] != -1:
-        data[y+1][x]+=1
-        if(data[y+1][x] >= 10):
-            data[y+1][x] = 0
-            spiral(y+1,x)
-    if data[y+1][x-1] != -1:
-        data[y+1][x-1]+=1
-        if(data[y+1][x-1] >= 10):
-            data[y+1][x-1] = 0
-            spiral(y+1,x-1)
-    if data[y][x+1] != -1:
-        data[y][x+1]+=1
-        if(data[y][x+1] >= 10):
-            data[y][x+1] = 0
-            spiral(y,x+1)
-    if data[y][x-1] != -1:
-        data[y][x-1]+=1
-        if(data[y][x-1] >= 10):
-            data[y][x-1] = 0
-            spiral(y,x-1)
-    if data[y-1][x+1] != -1:
-        data[y-1][x+1]+=1
-        if(data[y-1][x+1] >= 10):
-            data[y-1][x+1] = 0
-            spiral(y-1,x+1)
-    if data[y-1][x] != -1:
-        data[y-1][x]+=1
-        if(data[y-1][x] >= 10):
-            data[y-1][x] = 0
-            spiral(y-1,x)
-    if data[y-1][x-1] != -1:
-        data[y-1][x-1]+=1
-        if(data[y-1][x-1] >= 10):
-            data[y-1][x-1] = 0
-            spiral(y-1,x-1)
+def spiral(y0, x0):
+    global flashes
+    global flashed
+    spots = [(1, 1),(1, 0),(1,-1),
+             (0,1),(0,0),(0,-1),
+             (-1, 1),(-1, 0),(-1,-1)]
 
-for step in range(1, 3):
+
+    for y1, x1 in spots:
+        #print(y1,x1)
+        y=y0
+        x=x0
+        y+=y1
+        x+=x1
+        #print(y,x)
+        if (data[y][x] != -1 and 
+            not (y,x) in flashed):
+            data[y][x]+=1
+            if(data[y][x] >= 10):
+                data[y][x] = 0
+                flashes+=1
+                flashed.append(((y), (x)))
+                spiral(y,x)
+
+
+for step in range(1, 101):
     print('step', step)
-
+    flashed = []
     for y in range(1,len(data)-1):
         for x in range(1, len(data[y])-1):
             data[y][x]+=1
-
-
-    for y in range(1,len(data)-1):
-        for x in range(1, len(data[y])-1):
             if data[y][x]==10:
-                flashes+=1
+                #print('flash', y, x)
+                #flashes+=1
+                #flashed.append((y, x))
                 spiral(y, x)
-                data[y][x]=0
+                #data[y][x]=0
+                #print()
 
     pprint(data)
 print(flashes)
