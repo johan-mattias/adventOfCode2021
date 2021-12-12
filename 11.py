14b5d421b72f1d3e13adda46eb660007fcd0d123
Johan <johan.snider@gmail.com>
2021-12-12 13:05:13 +0100

    wow that was fun, i finally spent the time to setup my debugger and it was super worth it

--- 11.py
+++ 11.py
@@ -1,9 +1,17 @@
 from sys import stdin
 from pprint import pprint
+import sys 
+
+filename = '11_in.txt'
 
 data = []
-for line in stdin:
-    data.append([-1]+[int(char) for char in line.rstrip()]+[-1])
+
+with open(filename) as file:
+    for line in file:
+        data.append([-1]+[int(char) for char in line.rstrip()]+[-1])
+
+print(data)
+
 
 padding = [-1 for _ in data[0]]
 data.insert(0, padding)
@@ -11,14 +19,14 @@ data.append(padding)
 
 pprint(data)
 
+flashed = []
 flashes = 0
-flashed =[]
 
-def spiral(y0, x0):
+def flash(y0, x0):
     global flashes
     global flashed
     spots = [(1, 1),(1, 0),(1,-1),
-             (0,1),(0,0),(0,-1),
+             (0,1),(0,-1),
              (-1, 1),(-1, 0),(-1,-1)]
 
 
@@ -32,26 +40,43 @@ def spiral(y0, x0):
         if (data[y][x] != -1 and 
             not (y,x) in flashed):
             data[y][x]+=1
+
+    for y1, x1 in spots:
+        #print(y1,x1)
+        y=y0
+        x=x0
+        y+=y1
+        x+=x1
+        #print(y,x)
+        if (data[y][x] != -1 and 
+            not (y,x) in flashed):
             if(data[y][x] >= 10):
+
                 data[y][x] = 0
                 flashes+=1
                 flashed.append(((y), (x)))
-                spiral(y,x)
+                flash(y,x)
 
 
-for step in range(1, 101):
+for step in range(1, 300):
     print('step', step)
     flashed = []
     for y in range(1,len(data)-1):
         for x in range(1, len(data[y])-1):
             data[y][x]+=1
-            if data[y][x]==10:
+    for y in range(1,len(data)-1):
+        for x in range(1, len(data[y])-1):
+            if data[y][x]>=10:
                 #print('flash', y, x)
-                #flashes+=1
-                #flashed.append((y, x))
-                spiral(y, x)
-                #data[y][x]=0
+                flashes+=1
+                flashed.append((y, x))
+                data[y][x]=0
+                flash(y, x)
                 #print()
 
     pprint(data)
-print(flashes)
+    print(sum(map(sum, data)))
+    if sum(map(sum, data)) == -44:
+        print(step)
+        exit()
+#print(flashes)
